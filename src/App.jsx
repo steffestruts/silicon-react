bf6118c65c9b7be4950086a1143140a492c3ff40
Stefan Sjöberg <bird.nerd@outlook.com>
2024-10-24 13:15:19 +0200

    Gått igenom dagens utbildningsvideos om React-router och statisk HTML

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <>
-      <p>Testadadadadad</p>
-      <p>Test igen!</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur ratione accusantium, eveniet facilis dolorum quod molestias ipsam! In ullam debitis rem facere minima mollitia quos illum rerum harum ad, saepe vitae hic quaerat ipsum ea commodi quas eum vero facilis itaque, voluptatem, illo porro alias. Officiis quo quibusdam ratione laborum. Praesentium blanditiis dolores delectus rem, quis magnam voluptatem adipisci vel rerum ab perferendis ullam magni libero autem quaerat maiores explicabo beatae nulla eos? Illum soluta nam, eveniet rem itaque, atque totam veniam perferendis nihil sed inventore corrupti vero id ullam sunt nesciunt quia temporibus dicta earum perspiciatis? Rem ex iure asperiores officia nihil possimus esse dolor quasi soluta optio dolorem, illo recusandae fugit reiciendis porro atque incidunt aliquid fugiat reprehenderit deserunt natus consequatur labore! Sapiente, laboriosam. Impedit aut quos quia incidunt, rerum obcaecati maxime aliquam ab maiores possimus! Consectetur unde dignissimos reprehenderit, error eos minus porro tenetur aspernatur non eum accusantium tempore voluptate, deleniti possimus blanditiis molestiae, cum quis sed nostrum magnam molestias saepe soluta. Possimus, commodi. Repellat consequatur cum beatae soluta accusamus ipsa deserunt, ipsum laboriosam at sed praesentium! Vero debitis nobis adipisci fugit, in autem corporis reprehenderit eaque, nisi, fuga dolore magnam illum. Nihil, commodi ut. Voluptates.</p>
+
     </>
   )
 }
